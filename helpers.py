6b06f02f2c8932b9a6cec8303811a12af22641b0
dareto-dream <dare.to.0ream@gmail.com>
2026-02-10 17:04:20 -1000

    fix div injection

--- helpers.py
+++ helpers.py
@@ -30,34 +30,54 @@ def safe_path(path_str):
 
 def walk(directory):
     """
-    Generate an HTML file tree for the given directory
+    Generate an HTML file tree for the given directory.
+    
+    The JS extractFileTreeFromElement() parser expects:
+      - <div class="dir" data-name="..."><span class="dir-name">...</span></div>
+        followed by <div class="children">...</div> for subdirectories
+      - <div class="file" data-path="..." data-name="...">
+          <span class="file-name" data-path="...">...</span>
+          <span class="file-size">...</span>
+        </div> for files
     """
-    def _walk_recursive(dir_path, indent=0):
+    def _format_size(size_bytes):
+        if size_bytes < 1024:
+            return f"{size_bytes} B"
+        elif size_bytes < 1024 * 1024:
+            return f"{size_bytes / 1024:.1f} KB"
+        else:
+            return f"{size_bytes / (1024 * 1024):.1f} MB"
+
+    def _walk_recursive(dir_path):
         html_parts = []
         try:
             items = sorted(dir_path.iterdir(), key=lambda x: (not x.is_dir(), x.name))
             for item in items:
                 rel_path = item.relative_to(FILES_DIR)
-                indent_str = "  " * indent
-                
+                name = item.name
+
                 if item.is_dir():
-                    html_parts.append(f'{indent_str}<li class="dir">{item.name}/')
-                    html_parts.append(f'{indent_str}  <ul>')
-                    html_parts.append(_walk_recursive(item, indent + 2))
-                    html_parts.append(f'{indent_str}  </ul>')
-                    html_parts.append(f'{indent_str}</li>')
+                    html_parts.append(
+                        f'<div class="dir" data-name="{name}">'
+                        f'<span class="dir-name">{name}</span>'
+                        f'</div>'
+                    )
+                    children_html = _walk_recursive(item)
+                    if children_html:
+                        html_parts.append(f'<div class="children">{children_html}</div>')
                 else:
                     file_size = item.stat().st_size
-                    size_kb = file_size / 1024
+                    size_str = _format_size(file_size)
+                    path_str = str(rel_path)
                     html_parts.append(
-                        f'{indent_str}<li class="file">'
-                        f'<a href="/files/{rel_path}">{item.name}</a> '
-                        f'<span class="size">({size_kb:.1f} KB)</span>'
-                        f'</li>'
+                        f'<div class="file" data-path="{path_str}" data-name="{name}">'
+                        f'<span class="file-name" data-path="{path_str}">{name}</span>'
+                        f'<span class="file-size">{size_str}</span>'
+                        f'</div>'
                     )
         except PermissionError:
-            html_parts.append(f'{indent_str}<li class="error">Permission denied</li>')
-        
+            html_parts.append('<div class="error">Permission denied</div>')
+
         return '\n'.join(html_parts)
-    
-    return f'<ul class="file-tree">\n{_walk_recursive(directory)}\n</ul>'
+
+    return _walk_recursive(directory)
